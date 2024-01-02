e7b77e9826ba6ed6b40397b51e0aea61f392278a
emayom <emayom7@gmail.com>
2024-01-03 03:01:22 +0900

    :wrench: Chore: vite-plugin-svgr 패키지 설치 및 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [
+    svgr({
+      include: "**/*.svg?react",
+    }),
+    react(),
+    vanillaExtractPlugin(),
+  ],
   base: "/",
 });
