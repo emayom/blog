ad9371de5a70f3198cea3886443c14b9fa6fe53d
emayom <emayom7@gmail.com>
2024-01-07 07:51:36 +0900

    :lipstick: Design: vars 분리

--- src/components/layout/Footer/styles.css.ts
+++ src/components/layout/Footer/styles.css.ts
@@ -1,5 +1,6 @@
 import { style } from "@vanilla-extract/css";
 import { border, text } from "../../../styles/styles.css";
+import { vars } from "../../../styles/theme.css";
 
 export const footer = style([
   border,
@@ -8,8 +9,8 @@ export const footer = style([
     userSelect: "none",
     padding: "1rem",
     marginTop: "2rem",
-    color: "#6B7280",
-    background: "#f5f6f8",
+    color: vars.color.secondary,
+    background: vars.background.secondary,
     borderTopWidth: "1px",
   },
 ]);
