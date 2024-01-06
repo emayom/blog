cd9c1f1d440ae32a8761c7bf3eccaeb2c620837b
emayom <emayom7@gmail.com>
2024-01-07 07:56:55 +0900

    :sparkles: Feat: 동적 CSS Variables 생성 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { setElementVars } from "@vanilla-extract/dynamic";
+
+import { useThemeContext } from "./context/ThemeContext";
 
 import { Header, Footer } from "./components/layout";
 import { Home, About } from "./components/pages";
+
+import { vars } from "./styles/theme.css";
+import { darkTheme, lightTheme } from "./theme";
 import "./App.css";
 
 function App() {
+  const { isDark } = useThemeContext();
+
+  useEffect(() => {
+    setElementVars(
+      document.body,
+      vars,
+      isDark ? darkTheme : lightTheme
+    );
+  }, [isDark]);
+
   return (
     <div id="app">
       <Header />
