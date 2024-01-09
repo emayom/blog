6625d42873b71223028c1369120d878e27f946fd
emayom <emayom7@gmail.com>
2024-01-10 07:37:28 +0900

    :wrench: Chore: base 옵션 추가

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { ThemeContextProvider } from "./context/ThemeContext.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </ThemeContextProvider>
