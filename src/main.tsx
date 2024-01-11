c7469726f84419e7528b1cb53b01b2d122667a40
emayom <emayom7@gmail.com>
2024-01-11 23:40:16 +0900

    :art: Style: console.log 라인 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeContextProvider } from "./context/ThemeContext.tsx";
 
-console.log(import.meta.env.BASE_URL)
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeContextProvider>
