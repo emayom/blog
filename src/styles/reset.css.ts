a6a6bfbafea2bd7703e4f66ae5042aa8ef10b702
emayom <emayom7@gmail.com>
2024-01-13 03:58:48 +0900

    :adhesive_bandage: Fix: reset 스타일 수정

--- src/styles/reset.css.ts
+++ src/styles/reset.css.ts
@@ -1,13 +1,16 @@
 import { globalStyle, GlobalStyleRule } from "@vanilla-extract/css";
 import { reset } from "./layers.css.ts";
 
+/**
+ * h1, h2, h3, h4, h5, h6, p, blockquote, strong, sub, sup 제외
+ */
 export const globalReset: {
   selector: string;
   rule: GlobalStyleRule;
 }[] = [
   {
     selector:
-      "html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed,  figure, figcaption, footer, header, hgroup,  menu, nav, output, ruby, section, summary, time, mark, audio, video",
+      "html, body, div, span, applet, object, iframe, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed,  figure, figcaption, footer, header, hgroup,  menu, nav, output, ruby, section, summary, time, mark, audio, video",
     rule: {
       margin: 0,
       padding: 0,
@@ -33,7 +36,7 @@ export const globalReset: {
   {
     selector: "ol, ul",
     rule: {
-      listStyle: "none",
+      paddingLeft: "2rem",
     },
   },
   {
@@ -56,8 +59,9 @@ export const globalReset: {
     },
   },
   {
-    selector: "a",
+    selector: "a, button",
     rule: {
+      cursor: "pointer",
       textDecoration: "none",
       color: "inherit",
     },
