adbe84e75a55834ccad7d1b75fc3f63dd3cff703
emayom <emayom7@gmail.com>
2024-01-13 04:06:03 +0900

    :adhesive_bandage: Fix: theme context명 변경으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,35 @@ import { useThemeContext } from "./context/ThemeContext";
 import { Header, Footer } from "./components/layout";
 import { Home, About, Article } from "./components/pages";
 
-import { vars } from "./styles/theme.css";
-import { darkTheme, lightTheme } from "./theme";
+import { themeVars } from "./styles/theme.css";
+import { darkTheme, lightTheme } from "./theme/index";
 import "./App.css";
 
 function App() {
   const { isDark } = useThemeContext();
 
   useEffect(() => {
-    setElementVars(document.body, vars, isDark ? darkTheme : lightTheme);
+    setElementVars(document.body, themeVars, isDark ? darkTheme : lightTheme);
   }, [isDark]);
 
   return (
     <div id="app">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/article/:title" element={<Article />} />
-      </Routes>
+      <main
+        style={{
+          width: "100%",
+          maxWidth: "760px",
+          margin: "0 auto",
+          boxSizing: "border-box",
+          padding: "2rem",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/article/:title" element={<Article />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
