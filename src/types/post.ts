70ec7a8e0f5bc605894c9fd556e763d80201a33e
Ayoung Lim <ayoounglim@gmail.com>
2026-06-18 12:45:32 +0900

    feat(mdx): Post 타입 정의 추가
    PostFrontmatter / PostMeta / Post 타입 분리
    
    #13

---
+++ src/types/post.ts
@@ -0,0 +1,18 @@
+import type { ReactElement } from 'react'
+
+export interface PostFrontmatter {
+  title: string
+  date: string
+  tags: string[]
+  draft: boolean
+  description: string
+  thumbnail: string
+}
+
+export interface PostMeta extends PostFrontmatter {
+  slug: string
+}
+
+export interface Post extends PostMeta {
+  content: ReactElement
+}
